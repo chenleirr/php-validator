b7b8aeee0a1a24304a6cd672fd3b70d496866812
陈雷 <chenlei1@panda.tv>
2018-03-30 17:50:50 +0800

    someting changed

--- config.php
+++ config.php
@@ -9,15 +9,8 @@
  */
 
 return [
-
-    /*
-     * language file path base on root path of project
-     */
-    'lang' => 'lang/php-validator/zh_cn.php',
-
     /*
      * About scale of this PHP Component for 'max' or 'min' calculation rule
      */
     'scale' => 8
-
 ];
